3aa9490e95358d7d7b3b559c060e72b6de3fa303
keisuke-yanagi <yanagi@hep-th.phys.s.u-tokyo.ac.jp>
2018-06-10 22:10:28 +0900

    Faster computation for error and train

--- matrix_factorization/matrix_factorization.py
+++ matrix_factorization/matrix_factorization.py
@@ -2,10 +2,10 @@ import numpy as np
 
 class MatrixFactorization():
     """
-    This class receives a matrix M which contains rating data of the form 
+    This class receives a matrix R which contains rating data of the form 
     R[user_id, item_id] = rating of item[item_id] by user[user_id],
-    and factorize M into user characterstic matrix U and item cahracteristic matrix V;
-    R = U.V with bias vectors of users (bu) and items (bv).
+    and factorize R into user characterstic matrix U and item cahracteristic matrix V;
+    R = UV with bias vectors of users (bu) and items (bv).
     """
 
     def __init__(self, R, K, a=2.e-4, b=0.02):
@@ -24,12 +24,13 @@ class MatrixFactorization():
         self.__a = a
         self.__b = b
 
-        # Get the number of user and that of item.
+        # Get the number of user and of item.
         self.__user_size = self.__R.shape[0]
         self.__item_size = self.__R.shape[1]
 
         #Compute mean rating mu
         self.__mu = np.sum(self.__R)/np.sum(self.__R > 0)
+        self.__obs_index = (self.__R != 0).astype(int)
 
     def sgd(self, iu, ii):
         eui = self.__R[iu,ii] \
@@ -41,43 +42,30 @@ class MatrixFactorization():
 
     def error(self):
         error = 0.0
-        ius, iis = self.__R.nonzero()
-        for iu, ii in zip(ius, iis):
-            error = error + (self.__R[iu, ii] - (self.__mu + self.__bu[iu] + self.__bi[ii] + np.dot(self.__U[iu], self.__V[ii])))**2
-            error = error + self.__b/2*(np.sum(self.__U[iu]**2) + np.sum(self.__V[ii]**2) + self.__bu[iu]**2 + self.__bi[ii]**2)
+        Rhat = self.__mu + self.__bu[:, np.newaxis] + self.__bi[np.newaxis, :] + np.dot(self.__U, np.transpose(self.__V))
+        error = np.sum(((self.__R - Rhat) * self.__obs_index)**2)
+        error = error + self.__b/2*(np.sum(self.__U**2) + np.sum(self.__V**2) + np.sum(self.__bu**2) + np.sum(self.__bi**2))
         return np.sqrt(error)
         
-    def train(self, tol=1.0, step=1000, debug=False):
+    def train(self, atol=1.0, maxitr=2000000, step=1000, debug=False):
         self.__U = np.random.rand(self.__user_size, self.__K)
         self.__V = np.random.rand(self.__item_size, self.__K)
         self.__bu = np.zeros(self.__user_size)
         self.__bi = np.zeros(self.__item_size)
 
-        #For and while, whcih is faster??
-        # ius, iis = self.__R.nonzero()
-        # for i in range(2000000):
-        #     i_rand = np.random.randint(0, np.size(ius))
-        #     self.sgd(iu=ius[i_rand], ii=iis[i_rand])
-        #     if i % 1000 == 0:
-        #         print(i, self.error())
-        #     i = i + 1
-    
-        iiter = 0
-        diff = tol*1000
+        diff = atol*1000
         err_new = self.error()
         ius, iis = self.__R.nonzero()
-        while diff > tol:
+        for i in range(maxitr):
             i_rand = np.random.randint(0, np.size(ius))
             self.sgd(iu=ius[i_rand], ii=iis[i_rand])
-            if iiter % step ==0 and iiter > step:
+            if i % step == 0 and i>0:
                 err_old = err_new
                 err_new = self.error()
+                if debug is True: print(i, err_new)
                 diff = np.fabs(err_new - err_old)
-                if debug is True:
-                    print(iiter, err_new, diff)
-            iiter = iiter + 1
-        if debug is True:
-            print("Total iteration is ", iiter, "Resultant mean square error is ", err_new)
+                if diff < atol: break
+            i = i + 1
 
     @property
     def U(self):
@@ -94,8 +82,16 @@ class MatrixFactorization():
     @property
     def bi(self):
         return self.__bi
+
+    def rating(self, iu, ii):
+        return self.__mu + self.__bu[iu] + self.__bi[ii] + np.dot(self.__U[iu], self.__V[ii])
+
+    def rating_matrix(self):
+        return self.__mu + self.__bu[:, np.newaxis] + self.__bi[np.newaxis, :] + np.dot(self.__U, np.transpose(self.__V))
             
 if __name__ == "__main__":
     mf = MatrixFactorization(np.array([[1,0, 0, 4], [0,3,2,2], [3,4,0,0]]), 10)
-    mf.train(tol=1.e-3, step=1000)
-    print(mf.U, mf.V, mf.bu, mf.bi)
+    mf.train(atol=1.e-3, step=1000)
+    #print(mf.U, mf.V, mf.bu, mf.bi)
+    print(mf.rating(0,1))
+    print(mf.rating_matrix())
