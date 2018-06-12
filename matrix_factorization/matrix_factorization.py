a0c0847afcce2cd131e07fee32677394c565cf1f
keisuke-yanagi <yanagi@hep-th.phys.s.u-tokyo.ac.jp>
2018-06-12 21:32:50 +0900

    Tune parameters, make error function of test

--- matrix_factorization/matrix_factorization.py
+++ matrix_factorization/matrix_factorization.py
@@ -8,31 +8,37 @@ class MatrixFactorization():
     R = UV with bias vectors of users (bu) and items (bv).
     """
 
-    def __init__(self, R, K, a=0.001, b=0.02):
+    def __init__(self, R, R_test, K=10, a=0.01, b=0.2):
         """
         Input variables:
         - R (np.array) : Rating data R[user_id, item_id] used for training.
                          The rating score should be 1, 2, 3, 4 or 5.
                          Missing data should be set at 0.
+        - R_test : Rating data for test
         - K (int) : the size of user vector U[i] and item vector V[j].
         - a (float) : parameter for optimization. 
         - b (float) : parameter for regularization.
         """
         
         self.__R = R
+        self.__R_test = R_test
         self.__K = K
         self.__a = a
         self.__b = b
+
+        self.__train_size = np.sum(self.__R > 0)
+        self.__test_size = np.sum(self.__R_test > 0)
 
         # Get the number of user and of item.
         self.__user_size = self.__R.shape[0]
         self.__item_size = self.__R.shape[1]
 
         # Compute mean rating mu
-        self.__mu = np.sum(self.__R)/np.sum(self.__R > 0)
+        self.__mu = np.sum(self.__R)/self.__train_size
 
         # Stores obsered element of R as 1, and missing element as 0
         self.__obs_index = (self.__R != 0).astype(int)
+        self.__test_index = (self.__R_test != 0).astype(int)
 
     def sgd(self, iu, ii):
         """
@@ -58,12 +64,29 @@ class MatrixFactorization():
         Rhat = self.__mu + self.__bu[:, np.newaxis] + self.__bi[np.newaxis, :] + np.dot(self.__U, np.transpose(self.__V))
         
         # Squared error consisting only of the observed element of rating matrix R.
-        error = np.sum(((self.__R - Rhat) * self.__obs_index)**2)
+        error = np.sum(((self.__R - Rhat) * self.__obs_index)**2)/self.__train_size
         
         # Add regularization terms
         error = error + self.__b/2*(np.sum(self.__U**2) + np.sum(self.__V**2) + np.sum(self.__bu**2) + np.sum(self.__bi**2))
         
-        return np.sqrt(error)
+        return error
+
+    def error_test(self):
+        """
+        Compute sqrt(mean squared error), which is minimized by sgd method.
+        """
+        error = 0.0
+
+        # Rhat is predicted rating matrix
+        Rhat = self.__mu + self.__bu[:, np.newaxis] + self.__bi[np.newaxis, :] + np.dot(self.__U, np.transpose(self.__V))
+        
+        # Squared error consisting only of the observed element of rating matrix R.
+        error = np.sum(((self.__R_test - Rhat) * self.__test_index)**2)/self.__test_size
+        
+        # Add regularization terms
+        error = error + self.__b/2*(np.sum(self.__U**2) + np.sum(self.__V**2) + np.sum(self.__bu**2) + np.sum(self.__bi**2))
+        
+        return error
         
     def train(self, tol=1.e-3, maxitr=2000000,  debug=False):
         """
@@ -111,7 +134,7 @@ class MatrixFactorization():
             err_old = err_new
             err_new = self.error()
             ratio = err_new/err_old
-            if debug is True: print(i, 1-ratio)
+            if debug is True: print(i, 1-ratio, err_new, self.error_test())
             if np.fabs(ratio - 1.0) < tol: break
 
         # Naive implementation
