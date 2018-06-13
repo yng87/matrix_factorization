5a73ccd6367774de263f815b9f59e1562780fe10
keisuke-yanagi <yanagi@hep-th.phys.s.u-tokyo.ac.jp>
2018-06-13 23:11:45 +0900

    Arange for pip

--- setup.py
+++ setup.py
@@ -4,11 +4,9 @@ setup(
     name='matrix_factorization',
     version='0.0.1',
     description='Movie recommendation based on matrix factorization',
-    long_description=readme,
+    long_description="READNE.md",
     author='Keisuke Yanagi',
     author_email='k.yanagi07@gmail.com',
-    install_requires=['numpy', 'pandas', 'matplotlib'],
+    install_requires=['numpy', 'pandas'],
     url='https://github.com/keisuke-yanagi/matrix_factorization',
-    license=license,
-    packages=find_packages(exclude=('tests'))
 )
